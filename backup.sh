8700d48b6ba0d67c16c90d3a24226d67a156581f
masoud <masoud@host1.localdomain>
2023-06-09 10:13:58 +0330

    its for backup a file to /tmp/backup

---
+++ backup.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+read -p "Enter the path to the file that you want to backup: " backup_path
+
+backup() {
+  if [ -f "$1" ]; then
+    mkdir -p /tmp/backup
+    local backup_path="/tmp/backup/$(basename ${1}).$(date +%F).bk"
+    echo "Backing up $1 to ${backup_path}"
+    cp "$1" "$backup_path"
+    return 0
+  else
+    echo "ERROR: file doesn't exist"
+    return 1
+  fi
+}
+
+backup "$backup_path"
